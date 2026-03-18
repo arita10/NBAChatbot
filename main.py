1b2b487483139cb19840c6ae29027a9e1778761c
arita10 <balciapinya@gmail.com>
2026-03-18 18:37:37 +0300

    Add webhook router to main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routes.chat import router as chat_router
+from routes.webhook import router as webhook_router
 
 app = FastAPI(title="NBABot API")
 
 app.include_router(chat_router)
+app.include_router(webhook_router)
 
 
 @app.get("/")
